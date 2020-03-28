64a69317d4e31cd90a7ea1d4976dd1cfe1f6776f
J <dbc.raves@gmail.com>
2020-03-28 17:34:02 -0400

    Update DiscordBot.py
    Added Green Team role assign command and purge.

--- DiscordBot.py
+++ DiscordBot.py
@@ -13,7 +13,7 @@ class Bot(discord.Client):
 		self.message_channel = None
 		self.command_channel = None
 		self.error_channel = None
-		self.command_list = ['!absent', '!change', '!help', '!commands', '!info', '!raid', '!guildlraid' '!guild', '!invites', '!advertise', '!raidcheck', 'roleassign', '!pugpurge']
+		self.command_list = ['!absent', '!change', '!help', '!commands', '!info', '!raid', '!guild', '!invites', '!advertise', '!raidcheck', '!roleassign', '!pugpurge']
 
 	async def on_ready(self):
 		for guild in self.guilds:
@@ -68,9 +68,6 @@ class Bot(discord.Client):
 		async def raid(message):
 			await self.raid_conversation(message.author)
 
-		async def graid(message):
-				await self.graid_conversation(message.author)
-
 		async def guild(message):
 			await self.guild_conversation(message.author)
 
@@ -87,8 +84,7 @@ class Bot(discord.Client):
 			await self.message(self.command_channel, '**`!invites`** - Sends a message to everyone that has been added to the roster for one of the raids, letting them know they were invited.' +
 				'\n**`!advertise`** - Sends a message to everyone in the discord informing them about the upcoming raids and reminding them to apply.' +
 				'\n**`!pugpurge`** - purges all the members from a selected role.' +
-				'\n**`!roleassign`** - purges all the members from a selected role.' +
-				'\n**`!guildraid`** - Apply to a guild raid.')
+				'\n**`!roleassign`** - purges all the members from a selected role.')
 
 		async def pugpurge(message):
 			await self.pugurge_conversation(message.author)
@@ -108,9 +104,6 @@ class Bot(discord.Client):
 
 		elif message.content == '!raid':
 			await raid(message)
-
-		elif message.content == '!guildraid':
-			await graid(message)
 
 		elif message.content == '!guild':
 			await guild(message)
@@ -165,20 +158,11 @@ class Bot(discord.Client):
 			while(conversing):
 				await self.message(user, StringMachine.raids_recruitment(my_raiding.raid_list, my_raiding.metadata))
 
-				conversing, type = await self.conversation('Which type would you like to apply for? Please select a raid. **MC**, **BWL**, or **Cancel** (case is not important)', user, check, 120.0)
-				type.content = type.content.lower()
-				while type.content != 'mc' and type.content != 'bwl' and type.content != 'cancel':
-					conversing, type = await self.conversation('Please provide one of the following valid commands:\n**MC** - Apply to one of our MC raids.\n**BWL** - Apply to one of our BWL raids.\n**Cancel** - Cancel the application.', user, check, 120.0)
-					type.content = type.content.lower()
-				if type.content == 'cancel':
-					type.content = 'Cancel'
-				if type.content == 'mc':
-					type.content = 'MC'
-				if type.content == 'bwl':
-					type.content = 'BWL'
-				if type.content == 'Cancel':
-					await self.message(user, 'Your application has been cancelled!')
-					return
+				conversing, type = await self.conversation('Which type would you like to apply for? Please type the name of the type. (ex. CurrentWeek, or NextWeek)', user, check, 120.0)
+				if type.content == 'currentweek':
+					type.content = 'CurrentWeek'
+				if type.content == 'special':
+					type.content = 'Special'
 				if not self.controller.check_type_exists(type.content):
 					await self.message(user, 'That type does not exist! Please try again.')
 					return
@@ -186,109 +170,15 @@ class Bot(discord.Client):
 					application.update({'type': type.content})
 
 				conversing, response = await self.conversation('You have selected the following:\n\n{0}\n\nIs this correct? Please type "Yes" or "No".'.format(StringMachine.raids_info_for_type(my_raiding.raid_list, my_raiding.metadata, type.content)), user, check, 60.0)
-				response.content = response.content.lower()
-				if response.content == 'yes':
+				if response.content == 'yes' or response.content == 'Yes':
 					for raid in my_raiding.raid_list:
 						if raid.type == type.content:
 							conversing, response = await self.conversation('Would you be able to attend the following Raid? Please type "Yes" or "No".\n{0}'.format(StringMachine.raid_info(raid)), user, check, 60.0)
-							response.content = response.content.lower()
-							if response.content == 'yes':
+							if response.content == 'yes' or response.content == 'Yes':
 								application.update({raid.number: '{0} {1}'.format(raid.date, raid.time)})
 				else:
 					await self.message(user, 'Your application has been cancelled!')
 					return
-
-				i = 1
-				signed_up_for_any_raid = False
-				for i in range(i, num_raids+1):
-					signed_up_for_raid = application.get(i, '')
-					if signed_up_for_raid != '':
-						signed_up_for_any_raid = True
-				if not signed_up_for_any_raid:
-					await self.message(user, 'You didn\'t sign up for any raids! Your application has been cancelled!')
-					return
-				if type.content == 'MC':
-					conversing, character_name = await self.conversation('What is the name of your character?', user, check, 60.0)
-					application.update({'character_name': character_name.content})
-					conversing, character_class = await self.conversation('What is your character\'s class?', user, check, 60.0)
-					application.update({'class': character_class.content})
-					conversing, character_role = await self.conversation('What is your character\'s role? (DPS, Tank, Healer)', user, check, 60.0)
-					application.update({'role': character_role.content})
-					conversing, character_guild = await self.conversation('What is the name of the Guild that your character is in?', user, check, 60.0)
-					application.update({'guild': character_guild.content})
-					conversing, soft_res_one = await self.conversation('What is the first item that you would like to soft reserve?', user, check, 120.0)
-					application.update({'soft_res_one': soft_res_one.content})
-					conversing, soft_res_two = await self.conversation('What is the second item that you would like to soft reserve?', user, check, 120.0)
-					application.update({'soft_res_two': soft_res_two.content})
-					application.update({'discord_name': '{0}#{1}'.format(user.name, user.discriminator)})
-
-				if type.content == 'BWL':
-					conversing, character_name = await self.conversation('What is the name of your character?', user, check, 60.0)
-					application.update({'character_name': character_name.content})
-					conversing, character_class = await self.conversation('What is your character\'s class?', user, check, 60.0)
-					application.update({'class': character_class.content})
-					conversing, character_role = await self.conversation('What is your character\'s role? (DPS, Tank, Healer)', user, check, 60.0)
-					application.update({'role': character_role.content})
-					conversing, character_guild = await self.conversation('What is the name of the Guild that your character is in?', user, check, 60.0)
-					application.update({'guild': character_guild.content})
-					conversing, soft_res_one = await self.conversation('What is the item you would like to soft reserve?', user, check, 120.0)
-					application.update({'soft_res_one': soft_res_one.content})
-					#conversing, soft_res_two = await self.conversation('What is the second item that you would like to soft reserve?', user, check, 120.0)
-					#application.update({'soft_res_two': soft_res_two.content})
-					application.update({'discord_name': '{0}#{1}'.format(user.name, user.discriminator)})
-
-
-				self.controller.add_applicant(application)
-				await self.message(user, 'Your application has been submitted! ' +
-					'If you have additional characters or would like to sign up for an additional week, please apply again with **`!raid`**.')
-				return
-
-
-
-		except AttributeError:
-			await self.message(user, 'Error. Please try again.')
-
-	async def graid_conversation(self, user):
-		def check(response):
-			return response.channel == user.dm_channel and response.author == user
-		def yes_check(response):
-			return response.channel == user.dm_channel and (response.content == 'yes' or response.content == 'Yes') and response.author == user
-
-		my_raiding = self.controller.get_raiding()
-		application = {}
-		num_raids = my_raiding.metadata.get('num_raids', 0)
-		application.update({'num_raids': num_raids})
-		conversing = True
-
-		try:
-			while(conversing):
-				await self.message(user, StringMachine.graids_recruitment(my_raiding.raid_list, my_raiding.metadata))
-
-				conversing, type = await self.conversation('If you would like to apply to one of the guild runs please respond with **Orange** or **Purple**. Otherwise type **cancel** to stop the application.', user, check, 120.0)
-				type.content = type.content.lower()
-				while type.content != 'orange' and type.content != 'purple' and type.content != 'cancel':
-					conversing, type = await self.conversation('Please provide one of the following valid commands:\n**Orange** - Apply to the raids under Orange team.\n**Purple** - Apply to the raids under purple team.\n**Cancel** - Cancel the application.', user, check, 120.0)
-				if type.content == 'cancel':
-					type.content = 'Cancel'
-				if type.content == 'orange':
-					type.content = 'Orange'
-				if type.content == 'purple':
-					type.content = 'Purple'
-				if type.content == 'Cancel':
-					await self.message(user, 'Your application has been cancelled!')
-					return
-				if not self.controller.check_type_exists(type.content):
-					await self.message(user, 'That type does not exist! Please try again.')
-					return
-				else:
-					application.update({'type': type.content})
-
-				for raid in my_raiding.raid_list:
-					if raid.type == type.content:
-						conversing, response = await self.conversation('Would you be able to attend the following Raid? Please type "Yes" or "No".\n{0}'.format(StringMachine.raid_info(raid)), user, check, 60.0)
-						response.content = response.content.lower()
-						if response.content == 'yes' or response.content == 'Yes':
-							application.update({raid.number: '{0} {1}'.format(raid.date, raid.time)})
 
 				i = 1
 				signed_up_for_any_raid = False
@@ -306,17 +196,17 @@ class Bot(discord.Client):
 				application.update({'class': character_class.content})
 				conversing, character_role = await self.conversation('What is your character\'s role? (DPS, Tank, Healer)', user, check, 60.0)
 				application.update({'role': character_role.content})
-				conversing, soft_res_one = await self.conversation('What item would like to soft reserve?', user, check, 120.0)
-				conversing, character_guild = await self.conversation('What team are you usually a part of: Orange or Purple?', user, check, 60.0)
+				conversing, character_guild = await self.conversation('What is the name of the Guild that your character is in?', user, check, 60.0)
 				application.update({'guild': character_guild.content})
+				conversing, soft_res_one = await self.conversation('What is the first item that you would like to soft reserve?', user, check, 120.0)
 				application.update({'soft_res_one': soft_res_one.content})
-				#conversing, soft_res_two = await self.conversation('What is the second item that you would like to soft reserve?', user, check, 120.0)
-				#application.update({'soft_res_two': soft_res_two.content})
+				conversing, soft_res_two = await self.conversation('What is the second item that you would like to soft reserve?', user, check, 120.0)
+				application.update({'soft_res_two': soft_res_two.content})
 				application.update({'discord_name': '{0}#{1}'.format(user.name, user.discriminator)})
 
 				self.controller.add_applicant(application)
 				await self.message(user, 'Your application has been submitted! ' +
-					'If you have additional characters or would like to sign up for an additional week, please apply again with **`!guildraid`**.')
+					'If you have additional characters or would like to sign up for an additional week, please apply again with **`!raid`**.')
 				return
 		except AttributeError:
 			await self.message(user, 'Error. Please try again.')
@@ -379,13 +269,11 @@ class Bot(discord.Client):
 				await self.message(self.command_channel, 'The following raid:\n{0}'.format(StringMachine.raid_info(my_raid)))
 				await self.message(self.command_channel, 'The following characters:\n{0}'.format(members_string))
 				conversing, response = await self.conversation('If this is acceptable, then please type "yes" to confirm.', self.command_channel, check, 60.0)
-				response.content = response.content.lower()
 				if response.content != 'Yes' and response.content != 'yes':
 					await self.message(self.command_channel, 'Invites have been cancelled.')
 					return
 
 				conversing, response = await self.conversation('Would you like to assign a role to this raid?\nType Yes or No (Or anything else really if you dont want to do it)', self.command_channel, check, 60.0)
-				response.content = response.content.lower()
 				if response.content == 'yes' or response.content == 'Yes':
 					conversing, response = await self.conversation('Please select the NUMBER of the following list of roles\n\n	1. MC Sunday Pug\n	2. MC Wednesday Pug\n\n If you no longer wish to assign a role then honestly just type anything else.', self.command_channel, check, 60.0)
 					if response.content == '1':
@@ -430,7 +318,7 @@ class Bot(discord.Client):
 			while conversing:
 				conversing, response = await self.conversation('Raiders will be invited to join the following:\n\n{0}\n\nIf this is acceptable, then please type "yes" to confirm.'.format(StringMachine.raids_recruitment(my_raid_list, my_metadata)),
 					self.command_channel, check, 60.0)
-				response.content = response.content.lower()
+
 				if response.content == 'yes' or response.content == 'Yes':
 					members_list = self.guild.members
 					for user in members_list:
@@ -476,7 +364,7 @@ class Bot(discord.Client):
 		conversing = True
 		try:
 			while conversing:
-				conversing, response = await self.conversation('Please select the NUMBER of the following list of roles you wish to remove the members of\n\n1. MC Sunday Pug\n2. MC Wednesday Pug\n\nIf you no longer wish to purge a role then honestly just type anything else.', self.command_channel, check, 60.0)
+				conversing, response = await self.conversation('Please select the NUMBER of the following list of roles you wish to remove the members of\n\n1. MC Sunday Pug\n2. MC Wednesday Pug\n3. Green Team\n\nIf you no longer wish to purge a role then honestly just type anything else.', self.command_channel, check, 60.0)
 				if response.content == '1':
 					role = discord.utils.get(user.guild.roles, name='MC Sunday Pug')
 					for member in members_list:
@@ -486,6 +374,13 @@ class Bot(discord.Client):
 					return
 				elif response.content == '2':
 					role = discord.utils.get(user.guild.roles, name='MC Wednesday Pug')
+					for member in members_list:
+						if role in member.roles:
+							await member.remove_roles(role)
+					await self.message(self.command_channel, 'All member from this role have been removed.')
+					return
+				elif response.content == '3':
+					role = discord.utils.get(user.guild.roles, name='Green Team')
 					for member in members_list:
 						if role in member.roles:
 							await member.remove_roles(role)
@@ -534,11 +429,10 @@ class Bot(discord.Client):
 				await self.message(self.command_channel, 'The following raid:\n{0}'.format(StringMachine.raid_info(my_raid)))
 				await self.message(self.command_channel, 'The following characters:\n{0}'.format(members_string))
 				conversing, response = await self.conversation('If this is acceptable, then please type "yes" to confirm.', self.command_channel, check, 60.0)
-				response.content = response.content.lower()
 				if response.content != 'Yes' and response.content != 'yes':
 					await self.message(self.command_channel, 'Raid assignment has been cancelled.')
 					return
-				conversing, response = await self.conversation('Please select the NUMBER of the following list of roles\n\n	1. MC Sunday Pug\n	2. MC Wednesday Pug\n\n If you no longer wish to assign a role then honestly just type anything else.', self.command_channel, check, 60.0)
+				conversing, response = await self.conversation('Please select the NUMBER of the following list of roles\n\n1. MC Sunday Pug\n2. MC Wednesday Pug\n3. Green Team\n\n If you no longer wish to assign a role then honestly just type anything else.', self.command_channel, check, 60.0)
 				if response.content == '1':
 					rolename = 'MC Sunday Pug'
 					if all_members_exist:
@@ -549,6 +443,14 @@ class Bot(discord.Client):
 					return
 				elif response.content == '2':
 					rolename = 'MC Wednesday Pug'
+					if all_members_exist:
+						for character in my_raid.roster:
+							user = self.guild.get_member_named(character.discord)
+							await user.add_roles(discord.utils.get(user.guild.roles, name=rolename))
+					await self.message(self.command_channel, 'Role assigned\n')
+					return
+				elif response.content == '3':
+					rolename = 'Green Team'
 					if all_members_exist:
 						for character in my_raid.roster:
 							user = self.guild.get_member_named(character.discord)
@@ -580,8 +482,7 @@ class StringMachine:
 	def raids_info(raid_list, metadata):
 		str = ''
 		for type_key in metadata.get('types').keys():
-			if type_key == 'MC' or type_key == 'BWL':
-				str += StringMachine.raids_info_for_type(raid_list, metadata, type_key) + '\n\n'
+			str += StringMachine.raids_info_for_type(raid_list, metadata, type_key) + '\n\n'
 		# Remove last \n
 		if str != '':
 			str = str[:-2]
@@ -589,20 +490,6 @@ class StringMachine:
 
 	def raids_recruitment(raid_list, metadata):
 		str = 'Hello!\n<Adapt> is hosting the following Raids:\n{0}'.format(StringMachine.raids_info(raid_list, metadata))
-		return str
-
-	def graids_recruitment(raid_list, metadata):
-		str = 'Hello!\n<Adapt> is hosting the following Raids:\n{0}'.format(StringMachine.graids_info(raid_list, metadata))
-		return str
-
-	def graids_info(raid_list, metadata):
-		str = ''
-		for type_key in metadata.get('types').keys():
-			if type_key == 'Orange' or type_key == 'Purple':
-				str += StringMachine.raids_info_for_type(raid_list, metadata, type_key) + '\n\n'
-		# Remove last \n
-		if str != '':
-			str = str[:-2]
 		return str
 
 	def character_info(character):
